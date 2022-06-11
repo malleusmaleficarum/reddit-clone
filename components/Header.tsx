e1a05db94b8510fa9358545ea2dd91acd50e5ffd
binsar <binsar@asd.com>
2022-06-12 02:28:26 +0700

    header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
 import styles from "../styles/Header.module.css";
+import { ChevronDownIcon, HomeIcon, SearchIcon } from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+  UserIcon,
+  ArrowDownIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
@@ -12,6 +24,46 @@ const Header = () => {
           height={35}
           objectFit='contain'
         />
+      </div>
+      <div className={styles.middleside}>
+        <HomeIcon height={23} width={23} />
+        <p>Home</p>
+        <ChevronDownIcon height={23} width={23} />
+      </div>
+
+      <form className={styles.searchbar}>
+        <SearchIcon height={23} width={23} color={"rgb(156 163 175)"} />
+        <input type='text' placeholder='Search Reddit' />
+        <button type='submit' hidden />
+      </form>
+
+      <div className={styles.rightside}>
+        <SparklesIcon className={styles.icon} />
+        <GlobeIcon className={styles.icon} />
+        <VideoCameraIcon className={styles.icon} />
+        <hr />
+        <ChatIcon className={styles.icon} />
+        <BellIcon className={styles.icon} />
+        <PlusIcon className={styles.icon} />
+        <SpeakerphoneIcon className={styles.icon} />
+      </div>
+
+      {/* Hamburger Menu */}
+      <div className={styles.burger}>
+        <UserIcon className={styles.burger__icon} />
+        <ChevronDownIcon className={styles.icon__icon} />
+      </div>
+
+      {/* Log In */}
+      <div className={styles.login}>
+        <Image
+          src='https://links.papareact.com/23l'
+          alt='Reddit'
+          width={20}
+          height={20}
+          objectFit='contain'
+        />
+        <p>Sign In</p>
       </div>
     </div>
   );
